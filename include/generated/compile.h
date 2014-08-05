85d3b27c99df9d6d3774c1a3b067d20bc32d4b1c
Lucho Rocha <rocha.lucho@gmail.com>
2014-08-05 14:58:28 -0300

    make clean && make mrproper

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 23 15:23:17 KST 2014"
-#define LINUX_COMPILE_BY "shina.lee"
-#define LINUX_COMPILE_HOST "Sprint10"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
